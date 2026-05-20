6bc2c0864dbbd6b2a0780389122b088731c795a8
Roderic Wadds <rwadds@gmail.com>
2026-05-20 14:36:28 -0700

    Initial Commit

---
+++ mdread/mdreadApp.swift
@@ -0,0 +1,17 @@
+//
+//  mdreadApp.swift
+//  mdread
+//
+//  Created by Roderic Wadds on 5/20/26.
+//
+
+import SwiftUI
+
+@main
+struct mdreadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
